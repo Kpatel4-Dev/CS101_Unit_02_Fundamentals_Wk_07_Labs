cb03164232bd2d109b383f7b69425987ef2440d6
Kavita Patel <kpatel@miracosta.edu>
2025-08-03 19:29:46 -0700

    Create Branching_Looping.py

---
+++ Lab_02/Branching_Looping.py
@@ -0,0 +1,32 @@
+'''
+Lab 02 -- Branching and Looping
+
+Description: Demonstrate branching + looping in Python
+
+Developer Name(s): <your name>
+
+Date: <last date modified>
+'''
+##########################################
+# IMPORTS:
+#   modules needed for program
+##########################################
+
+
+##########################################
+# FUNCTIONS:
+#   functions useful to main program
+##########################################
+#code goes here
+
+
+##########################################
+# MAIN PROGRAM:
+#   beginning of running program
+##########################################
+#code goes here
+
+
+
+
+
